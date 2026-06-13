a99cfe40b433d39e3386452dd9a74959e252ad67
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-06-14 01:30:55 +0200

    feat: implement checkout with order persistence and transaction support

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -10,18 +10,16 @@ import {
   Inject,
 } from '@nestjs/common';
 
-import { Order, OrderService } from '../order';
 import { calculateCartTotal } from './models-rules';
 import { CartService } from './services';
 import { CartItem } from './models';
 import { CreateOrderDto, PutCartPayload } from '../order/type';
+import { pool } from '../shared/db/db';
+import { Order } from 'src/order/models';
 
 @Controller('api/profile/cart')
 export class CartController {
-  constructor(
-    @Inject(CartService) private cartService: CartService,
-    @Inject(OrderService) private orderService: OrderService,
-  ) {}
+  constructor(@Inject(CartService) private cartService: CartService) {}
 
   @Get()
   async findUserCart(): Promise<CartItem[]> {
@@ -53,32 +51,60 @@ export class CartController {
   async checkout(@Body() body: CreateOrderDto) {
     const userId = '11111111-1111-1111-1111-111111111111';
 
-    const cart = await this.cartService.findByUserId(userId);
+    await pool.query('BEGIN');
 
-    if (!(cart && cart.items.length)) {
-      throw new BadRequestException('Cart is empty');
+    try {
+      const cart = await this.cartService.findByUserId(userId);
+
+      if (!(cart && cart.items.length)) {
+        throw new BadRequestException('Cart is empty');
+      }
+
+      const { id: cartId, items } = cart;
+      const total = calculateCartTotal(items);
+
+      const { rows } = await pool.query(
+        `
+      INSERT INTO orders
+      (user_id, cart_id, payment, delivery, comments, status, total)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *
+      `,
+        [
+          userId,
+          cartId,
+          JSON.stringify({ type: 'card' }),
+          JSON.stringify({ address: body.address }),
+          '',
+          'CREATED',
+          total,
+        ],
+      );
+
+      await pool.query(
+        `
+      UPDATE carts
+      SET status = $1, updated_at = NOW()
+      WHERE id = $2
+      `,
+        ['ORDERED', cartId],
+      );
+
+      await pool.query('COMMIT');
+
+      return { order: rows[0] };
+    } catch (e) {
+      await pool.query('ROLLBACK');
+      throw e;
     }
-
-    const { id: cartId, items } = cart;
-    const total = calculateCartTotal(items);
-    const order = this.orderService.create({
-      userId,
-      cartId,
-      items: items.map(({ product, count }) => ({
-        productId: product.id,
-        count,
-      })),
-      address: body.address,
-      total,
-    });
-
-    await this.cartService.removeByUserId(userId);
-
-    return { order };
   }
 
   @Get('order')
-  getOrder(): Order[] {
-    return this.orderService.getAll();
+  async getOrders(): Promise<Order[]> {
+    const { rows } = await pool.query(`
+    SELECT * FROM orders ORDER BY created_at DESC
+  `);
+
+    return rows;
   }
 }
